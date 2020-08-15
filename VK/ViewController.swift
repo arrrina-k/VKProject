15f150a21a01e8bacfd685e8695a2823de36a063
arrrina-k <krivenko.arinka@yandex.ru>
2020-08-15 16:37:02 +0300

    Initial Commit

---
+++ VK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK
+//
+//  Created by Арина Кривенко on 15.08.2020.
+//  Copyright © 2020 Арина Кривенко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
