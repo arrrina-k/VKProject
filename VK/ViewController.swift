c22bca131b843ed88004a3224d6daa3ca9079f22
arrrina-k <krivenko.arinka@yandex.ru>
2020-08-16 15:49:09 +0300

    Задание к первому уроку

--- VK/ViewController.swift
+++ VK/ViewController.swift
@@ -10,9 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var enterButton: UIButton!
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super .viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardAppeared), name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    @objc
+    private func keyboardAppeared(notification: Notification) {
+        guard
+            let userInfo = notification.userInfo,
+            let keyboardSize = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {return}
+        print("keyboard shown \(keyboardSize.height)")
+        scrollView.contentInset.bottom = keyboardSize.height
+    }
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        let login = loginField.text
+        let password = passwordField.text
+        
+        if login == "admin", password == "12345" {
+            print("Вход выполнен")
+        } else {
+            print("Пароль или логин неверны")
+        }
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
